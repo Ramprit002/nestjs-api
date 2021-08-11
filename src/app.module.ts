feca2b5e5d87a213085d474acf4b31bbd3eab65c
ramprits <palak.ghai@torinit.ca>
2021-08-11 21:15:02 +0530

    Adding required files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TagModule } from './tag/tag.module';
+import { UserModule } from './user/user.module';
+import { ArticleModule } from './article/article.module';
+import ormconfig from './ormconfig';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    TagModule,
+    UserModule,
+    ArticleModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
